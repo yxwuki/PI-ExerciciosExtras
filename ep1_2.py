cec46885ac4c5e521a700903280ce81d945343ea
Gabriela Sei <93676975+yxwuki@users.noreply.github.com>
2022-06-20 14:57:11 -0300

    Add files via upload

---
+++ ep1_2.py
@@ -0,0 +1,6 @@
+n1 = float(input())
+n2 = float(input())
+n3 = float(input())
+
+media = (n1+n2+n3)/3
+print("%.2f" % media)
---
+++ ep1_3.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+
+soma = a+b
+sub = a-b
+multi = a*b
+div = a/b
+divInt = a//b
+mod = a%b
+
+print("%.0f" % soma, "\n%.0f" % sub, "\n%.0f" % multi, "\n%.1f" % div, "\n%.0f" % divInt, "\n%.0f" % mod)
